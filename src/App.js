51836c1c3069f1fa003ab476af1a2888b4521917
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-05-24 23:11:26 +0200

    review component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import people from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const [index, setIndex] = useState(0);
+    const {name, job, image, text} = people[index]
+
+    const checkNumber = (number) => {
+        if (number > people.length - 1) {
+            return 0;
+        }
+        if (number < 0) {
+            return people.length - 1;
+        }
+        return number;
+    }
+
+    const randomPerson = () =>{
+        let randomNumber = Math.floor(Math.random()*people.length)
+        if (randomNumber === index){
+            randomNumber = index + 1
+        }
+        setIndex(checkNumber(randomNumber))
+    }
+
+    const nextPerson = () => {
+        setIndex((currIndex) => {
+            const newIndex = currIndex + 1;
+            if (newIndex > people.length - 1) {
+                return 0;
+            }
+            return checkNumber(newIndex);
+        })
+    }
+    const prevPerson = () => {
+        setIndex((currIndex) => {
+            const newIndex = currIndex - 1;
+            if (newIndex < 0) {
+                return people.length - 1;
+            }
+            return checkNumber(newIndex);
+        })
+    }
+    return (
+        <main style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <article style={{
+                width: 500,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <img src={image} style={{width: 250, height: 250, objectFit: "cover"}} alt={name}/>
+                <h4>{name}</h4>
+                <p>{job}</p>
+                <p>{text}</p>
+                <div>
+                    <button type='button' onClick={nextPerson}>Next</button>
+                    <button type='button' onClick={randomPerson}>Random</button>
+                    <button type='button' onClick={prevPerson}>Prev</button>
+                </div>
+            </article>
+        </main>
+    );
 }
 
 export default App;
